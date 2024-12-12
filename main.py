7d9b8fbd7b4c55166a0fa7fa6d631c1ca6c751f6
Alex <alex70.07@mail.ru>
2024-12-12 18:49:52 +0300

    написана функция notify_weather

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from urllib import response
 import requests
 from plyer import notification
+temp = ''
+
+
 
 # константы 
 API_KEY = "23496c2a58b99648af590ee8a29c5348"
@@ -20,8 +23,6 @@ def get_weather(city: str, api_key: str):
     print(response.status_code) # Получили статус ответа
     weather_dict = response.json() # Получили JSON из объекта ответа
     return weather_dict # Получили объект Python из JSON
-    
-get_weather(CITY, API_KEY)
 
 def format_weather_message(weather_dict: dict):
     """
@@ -29,32 +30,35 @@ def format_weather_message(weather_dict: dict):
     Аргументы weather_dict: Словарь с данными о погоде.
     Возвращает str форматированное сообщение о погоде.
     """
-    temp = round(weather_dict['main']['temp'])
-    # Ощущается как
-    feels_like = round(weather_dict['main']['feels_like'])
-    # Описание погоды
-    description = weather_dict['weather'][0]['description']
-    # форматируем сообщение
-    message = 'Температура: {temp}°C\nОщущается как: {feels_like}°C\nОписание: {description}'
-    # возвращаем сообщение
-    return message
-
-format_weather_message(weather_dict)
+    global message
+    temp = round(weather_dict['main']['temp']) # температура
+    feels_like = round(weather_dict['main']['feels_like']) # Ощущается как
+    description = weather_dict['weather'][0]['description'] # Описание погоды
+    message = 'Температура: {temp}°C\nОщущается как: {feels_like}°C\nОписание: {description}' # форматируем сообщение
+    return message # возвращаем сообщение
 
 def notify_weather(message: str):
-    pass
     """ 
     Функция отправляет уведомление пользователю с информацией о погоде.
     - Аргументы: `message`: Сообщение о погоде для уведомления.
     Возвращает: `None`
     """
-    # return None
+    temp = round(weather_dict['main']['temp']) # температура
+    feels_like = round(weather_dict['main']['feels_like']) # Ощущается как
+    description = weather_dict['weather'][0]['description'] # Описание погоды
+    notification.notify(
+    title='Погода в Москве',
+    message=f'Температура: {temp}°C\nОщущается как: {feels_like}°C\nОписание: {description}',
+    app_name='Weather',
+    app_icon=None)
+    return None
 
 def main():
-    pass
     """
     Фукция запускает программу, выполняет вызовы вышеуказанных функций и обрабатывает вывод.
     """
     get_weather(CITY, API_KEY)
     format_weather_message(weather_dict)
     notify_weather(message)
+
+main()
